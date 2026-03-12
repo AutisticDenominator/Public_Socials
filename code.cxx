c3339b1d8fb5b833d20fa07e09d7259d3e0176c9
AutisticDenominator <AutisticDenominator@proton.me>
2026-03-12 17:38:27 +0100

    Main classes

--- code.cxx
+++ code.cxx
@@ -2,3 +2,34 @@
 #include <string>
 #include <map>
 #include <./http_library/cpp-httplib/httplib.h>
+using namespace std;
+
+class Persons{
+public:
+    string First_Name;
+    string Last_Name;
+    string Country;
+
+    Persons(string First_Name, string Last_Name, string Country){
+        this->First_Name = First_Name;
+        this->Last_Name = Last_Name;
+        this->Country = Country;
+    }
+};
+
+class Results{
+public:
+    string Username;
+    string Url;
+    string Page_Body;
+
+    Results(string Username, string Url, string Page_Body){
+        this->Username = Username;
+        this->Url = Url;
+        this->Page_Body = Page_Body;
+    }
+};
+
+int main(){
+    return 0;
+}
