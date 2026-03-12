b37f4464db1be3292ddfbe1164362467faf8636e
AutisticDenominator <AutisticDenominator@proton.me>
2026-03-12 17:51:59 +0100

    Default constructor definition

--- code.cxx
+++ code.cxx
@@ -4,29 +4,32 @@
 #include <./http_library/cpp-httplib/httplib.h>
 using namespace std;
 
-class Persons{
-public:
-    string First_Name;
-    string Last_Name;
-    string Country;
-
-    Persons(string First_Name, string Last_Name, string Country){
-        this->First_Name = First_Name;
-        this->Last_Name = Last_Name;
-        this->Country = Country;
-    }
-};
-
 class Results{
 public:
     string Username;
     string Url;
     string Page_Body;
 
+    Results() = default;
     Results(string Username, string Url, string Page_Body){
         this->Username = Username;
         this->Url = Url;
         this->Page_Body = Page_Body;
+    }
+};
+
+class Persons{
+public:
+    string First_Name;
+    string Last_Name;
+    string Country;
+
+    Results Result_List[256];
+
+    Persons(string First_Name, string Last_Name, string Country){
+        this->First_Name = First_Name;
+        this->Last_Name = Last_Name;
+        this->Country = Country;
     }
 };
 
